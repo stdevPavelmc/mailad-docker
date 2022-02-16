439b8578a052882bf882ef1d4da85c7915250fe5
Pavel Milanes (CO7WT) <pavelmc@gmail.com>
2022-02-15 21:03:39 -0500

    Complete the scaffolding of the Active directory with a structure to test every use case

--- samba/scaffolding.sh
+++ samba/scaffolding.sh
@@ -1,20 +1,6 @@
 #!/bin/sh
 set -e
 
-# create OU function
-create_ou()
-{
-    # $1: full OU name from the base of the LDAP
-    # $2: description [optional]
-
-    if [ -z "${2}" ] ; then
-        samba-tool ou create "${1}"
-        return
-    fi
-
-    samba-tool ou create "${1}" --description="${2}"
-}
-
 # create users function
 create_user()
 {
@@ -31,20 +17,45 @@ create_user()
         --userou="${5},OU=${SAMBA_LDAP_BASE_OU}"
 }
 
-# create groups
-create_groups()
-{
-    # $1: Group name
-    # $2: Description
-    # $3: email address []
 
-    samba-tool group "create" "${1}" \
-        --description="${2}" \
-        --mail-address="${3}" \
-        --groupou="${4}" \
-        -U "administrator" \
-        --password="${SAMBA_DC_ADMIN_PASSWD}"
+# create group
+create_group()
+{
+    # $1: Group name & description
+    # $2: group OU
+    # $3: email address [optional]
+
+    OPT=""
+    if [ "${3}" ] ; then
+        OPT="--mail-address='${3}'"
+    fi
+        samba-tool group "add" "${1}" \
+            --description="${1}" \
+            --groupou="${2}" ${OPT} \
+            -U "administrator" \
+            --password="${SAMBA_DC_ADMIN_PASSWD}"
 }
+
+# create OU function
+create_ou()
+{
+    # $1: full OU name from the base of the LDAP
+    # $2: description/Name
+    # $3: mail for the associated group [Optional]
+
+    # create the OU
+    samba-tool ou create "${1}" --description="${2}"
+
+    # optional mail
+    MAIL=""
+    if [ "${3}" ] ; then
+        MAIL="${3}"
+    fi
+
+    # create an associated group
+    create_group "${2}" "${1}" "${MAIL}"
+}
+
 
 # get the base OU for the users, is the first level of the domain
 SAMBA_LDAP_BASE_OU=`echo ${SAMBA_DC_DOMAIN} | cut -d "." -f 1 | tr [:lower:] [:upper:]`
@@ -54,12 +65,24 @@ samba-tool user create "${SAMBA_LINK_USER}" "${SAMBA_LINK_USER_PASSWORD}" --desc
 
 # Create the a simple structure in the LDAP
 create_ou "OU=${SAMBA_LDAP_BASE_OU}" "Base OU"
-create_ou "OU=Sysadmins,OU=${SAMBA_LDAP_BASE_OU}" "Sysadmins"
-create_ou "OU=ItSupport,OU=${SAMBA_LDAP_BASE_OU}" "IT's staff"
-create_ou "OU=Unpriv,OU=${SAMBA_LDAP_BASE_OU}" "Least privilege users"
+create_ou "OU=Sysadmins,OU=${SAMBA_LDAP_BASE_OU}" "Sysadmins" "sysadmins@${SAMBA_DC_DOMAIN}"
+create_ou "OU=ItSupport,OU=${SAMBA_LDAP_BASE_OU}" "ItSupport" "it@${SAMBA_DC_DOMAIN}"
+create_ou "OU=Unpriv,OU=${SAMBA_LDAP_BASE_OU}" "Unpriv" "dummy@${SAMBA_DC_DOMAIN}"
 
 # Create the mailadmin user
 create_user "${SAMBA_MAILADMIN_USER}" "${SAMBA_MAILADMIN_USER_PASSWD}" "MailAdmin" "OfAllThisShit" "OU=Sysadmins"
 create_user "niusdy" "N1usdy_" "Niusdania" "Itstaff" "OU=ItSupport"
 create_user "liam" "L14m_qwet" "Liam" "Milanes" "OU=Unpriv"
 create_user "lisi" "L1sI___" "Lisabel" "Milanes" "OU=Unpriv"
+
+# add users to groups
+samba-tool group addmembers "Sysadmins" "${SAMBA_MAILADMIN_USER}"
+samba-tool group addmembers "ItSupport" "niusdy"
+samba-tool group addmembers "Unpriv" "liam,lisi"
+
+# Create Special access groups
+create_ou "OU=MAIL_ACCESS,OU=${SAMBA_LDAP_BASE_OU}" "Mail Access Groups"
+create_group "Local_mail" "OU=MAIL_ACCESS,OU=${SAMBA_LDAP_BASE_OU}" "locals@${SAMBA_DC_DOMAIN}"
+create_group "National_mail" "OU=MAIL_ACCESS,OU=${SAMBA_LDAP_BASE_OU}" "nationals@${SAMBA_DC_DOMAIN}"
+samba-tool group addmembers "Local_mail" "liam"
+samba-tool group addmembers "National_mail" "lisi"
